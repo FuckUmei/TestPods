ff15e9badac82164d07018ece91ce0b3e2250a7a
Specter <sjh86126@yeah.net>
2016-09-20 15:23:24 +0800

    Initial Commit

---
+++ TestPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPods
+//
+//  Created by specter on 16/9/20.
+//  Copyright © 2016年 specter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPods
+//
+//  Created by specter on 16/9/20.
+//  Copyright © 2016年 specter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
